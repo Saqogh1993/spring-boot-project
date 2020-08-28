b12e3c9365159609dacf7754ea4715015595dea6
Sargis Ghazaryan <sargis.ghazaryan@bureauveritas.com>
2020-08-28 14:40:51 +0400

    Added testing endpoint
    Change-Id: Iaa49f7a430ad91aac53ddce11e0e0cd3dd023479

--- src/main/java/am/bv/springbootproject/SpringBootProjectApplication.java
+++ src/main/java/am/bv/springbootproject/SpringBootProjectApplication.java
@@ -2,12 +2,19 @@ package am.bv.springbootproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootProjectApplication.class, args);
     }
 
+    @GetMapping
+    public String isUp() {
+        return "Server is up and running";
+    }
 }
